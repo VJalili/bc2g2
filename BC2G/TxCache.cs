ce8b8b393f5f04f9260f9b5dc4cbfad0a9fda7be
vjalili <VJalili@users.noreply.github.com>
2022-01-13 19:20:28 -0800

    checkin.

---
+++ BC2G/TxCache.cs
@@ -0,0 +1,12 @@
+﻿using BC2G.Model;
+using System.Collections.Concurrent;
+
+namespace BC2G
+{
+    public class TxCache
+    {
+        public ConcurrentDictionary<string, Transaction> Utxo = new();
+
+        public ConcurrentDictionary<string, Transaction> RecentTx = new();
+    }
+}
