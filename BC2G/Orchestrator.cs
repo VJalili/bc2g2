c3a9fa2896dfea62c63ebc9c4f4beb76b6677ff2
VJalili <VJalili@users.noreply.github.com>
2022-03-16 19:50:52 -0400

    small refactor.

--- BC2G/Orchestrator.cs
+++ BC2G/Orchestrator.cs
@@ -154,11 +154,6 @@ namespace BC2G
 
         private bool TryGetBitCoinAgent(CancellationToken cT, out BitcoinAgent agent, out TxCache txCache)
         {
-#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
-            agent = null;
-            txCache = null;
-#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
-
             try
             {
                 txCache = new TxCache(_options.OutputDir, cT);
@@ -173,6 +168,11 @@ namespace BC2G
                 Logger.LogException(
                     $"Failed to create/access BitcoinAgent: " +
                     $"{e.Message}");
+
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+                agent = null;
+                txCache = null;
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
                 return false;
             }
         }
