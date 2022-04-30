e6e495e5135de15bf81d1a05e161d2ceb698ce02
VJalili <VJalili@users.noreply.github.com>
2022-04-29 21:48:27 -0400

    Add a method to convert tsv to np.memmap.

---
+++ node_embedding/graph_sampling/tsv_to_memmap_binary.py
@@ -0,0 +1,56 @@
+import numpy as np
+import sys
+
+
+DELIMITER = "\t"
+
+
+def main(filename):
+
+    # It is inefficient to read the entire file
+    # in order to get row and column counts.
+    # We could ask these as input arguments, but
+    # that is not a good UX. You could leave it
+    # as is for now, but hopefully, with migration
+    # to DB, all this will be alleviated.
+    row_count, col_count = 0, 0
+    print(f"\nCounting rows and columns in {filename} ... ", end="", flush=True)
+    with open(filename, "r") as f:
+        header = next(f)
+        col_count = len(header.split(DELIMITER))
+        for _ in f:
+            row_count += 1
+
+    print(f"Done! #cols={col_count:,} #rows={row_count:,}")
+
+    print(f"\nCreating an empty memory map file (binary) of "
+          f"shape=({row_count},{col_count}) ... ", end="", flush=True)
+    mem_map_filename = filename + ".dat"
+    mem_map = np.memmap(mem_map_filename,
+                        dtype='float32',
+                        mode='w+',
+                        shape=(row_count, col_count))
+    print("Done!")
+    print(f"\n*** Memory map filename: {mem_map_filename}")
+
+    print("\nPopulating the memory map file:")
+    row_counter = 0
+    with open(filename, "r") as f:
+        next(f)
+        for line in f:
+            mem_map[row_counter] = line.split(DELIMITER)
+            # TODO: do you need to flush at every line?!
+            mem_map.flush()
+            row_counter += 1
+            print(f"\r\t{row_counter:,} / {row_count:,} ({row_counter / row_count:.2%})", end="")
+
+    print("\nAll process completed successfully.")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Missing filename.")
+        exit()
+
+    main(sys.argv[1])
+
