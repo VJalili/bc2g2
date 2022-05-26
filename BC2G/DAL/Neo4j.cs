30daaa0196732d86aa29cf4ccb0817f9ea2640fa
VJalili <VJalili@users.noreply.github.com>
2022-05-25 21:10:25 -0400

    Add hello world exmaple of Neo4j.

---
+++ BC2G/DAL/Neo4j.cs
@@ -0,0 +1,65 @@
+﻿using Neo4j.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BC2G.DAL
+{
+    public class Neo4j : IDisposable
+    {
+        private bool _disposed = false;
+        private readonly IDriver _driver;
+
+        ~Neo4j() => Dispose(false);
+
+        public Neo4j(string uri, string user, string password)
+        {
+            _driver = GraphDatabase.Driver(uri, AuthTokens.Basic(user, password));
+        }
+
+        public async void PrintGreeting(string message)
+        {
+            using (var session = _driver.AsyncSession())
+            {
+                var greeting = session.WriteTransactionAsync(async tx =>
+                {
+                    var result = await tx.RunAsync("CREATE (a:Greeting) " +
+                                        "SET a.message = $message " +
+                                        "RETURN a.message + ', from node ' + id(a)",
+                        new { message });
+                    return result.SingleAsync().Result[0].As<string>();
+                });
+                Console.WriteLine(greeting);
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+            {
+                _driver?.Dispose();
+            }
+
+            _disposed = true;
+        }
+
+        public static void Run()
+        {
+            using (var greeter = new Neo4j("bolt://localhost:7687", "neo4j", "password"))
+            {
+                greeter.PrintGreeting("hello, world");
+            }
+        }
+    }
+}
