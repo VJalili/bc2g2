01f29bf57632a4606544dc3bff70a8830335ec19
VJalili <VJalili@users.noreply.github.com>
2022-07-27 04:40:15 -0400

    test support HDF5 export of the sampled graphs.

--- BC2G/DAL/GraphDB.cs
+++ BC2G/DAL/GraphDB.cs
@@ -2,6 +2,7 @@
 using BC2G.DAL.Bulkload;
 using BC2G.Graph;
 using BC2G.Model;
+using HDF5CSharp;
 using Neo4j.Driver;
 
 namespace BC2G.DAL
@@ -318,6 +319,9 @@ namespace BC2G.DAL
             var allEdgeFeatures = new List<List<double[]>>();
             var allPairIndices = new List<List<int[]>>();
 
+            string filename = Path.Combine(@"C:\Users\Hamed\Desktop", "testChunks.H5");
+            long fileId = Hdf5.CreateFile(filename);
+
             var rndRootNodes = await GetRandomNodes(rootNodesCount, rootNodesSelectProb);
 
             foreach (var rootNode in rndRootNodes)
@@ -330,6 +334,18 @@ namespace BC2G.DAL
                 allNodeFeatures.Add(nodeFeatures);
                 allEdgeFeatures.Add(edgeFeature);
                 allPairIndices.Add(pairIndices);
+
+
+                try
+                {
+                    var aaa = allNodeFeatures[0].ToArray();
+                    Hdf5.WriteObject(fileId, aaa, "test");
+                    Hdf5.CloseFile(fileId);
+                }
+                catch(Exception e)
+                {
+
+                }
             }
         }
 
