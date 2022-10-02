eddfdf0761ebcff8929e57a9b76feb5cea1ff127
VJalili <VJalili@users.noreply.github.com>
2022-10-01 20:09:42 -0400

    Improve on setting utxo.

--- BC2G/Model/Utxo.cs
+++ BC2G/Model/Utxo.cs
@@ -13,6 +13,8 @@ namespace BC2G.Model
     [Table("Utxo")]
     public class Utxo
     {
+        private const char _delimiter = ';';
+
         [Required]
         public string Id { set; get; } = string.Empty;
         [Required]
@@ -25,16 +27,38 @@ namespace BC2G.Model
         /// is defined as output.
         /// </summary>
         [Required]
-        public string CreatedIn { set; get; }
-        public int CreatedInCount { set; get; }
+        public string CreatedIn
+        {
+            set { _createdIn = value; }
+            get { return _createdIn; }
+        }
+        private string _createdIn;
+
+        public int CreatedInCount
+        {
+            set { _createdInCount = value; }
+            get { return _createdInCount; }
+        }
+        private int _createdInCount;
 
         /// <summary>
         /// A list of comma-separated block heights where this txo
         /// is defined as input. If this list is empty, the tx is 
         /// unspent (utxo).
         /// </summary>
-        public string ReferencedIn { set; get; }
-        public int ReferencedInCount { set; get; }
+        public string ReferencedIn
+        {
+            set { _refdIn = value; }
+            get { return _refdIn; }
+        }
+        private string _refdIn;
+
+        public int ReferencedInCount
+        {
+            set { _refdInCount = value; }
+            get { return _refdInCount; }
+        }
+        private int _refdInCount;
 
         // This constructor is required by EF.
         public Utxo(string id, string address, double value, string createdIn, string referencedIn = "")
@@ -44,6 +68,11 @@ namespace BC2G.Model
             Value = value;
             CreatedIn = createdIn;
             ReferencedIn = referencedIn;
+
+            if (!string.IsNullOrEmpty(createdIn))
+                CreatedInCount = 1;
+            if (!string.IsNullOrEmpty(referencedIn))
+                ReferencedInCount = 1;
         }
 
         public Utxo(string txid, int voutN, string address, double value, string createdIn, string referencedIn = "") :
@@ -57,18 +86,27 @@ namespace BC2G.Model
 
         public void AddReferencedIn(string address)
         {
-            ReferencedInCount++;
-            if (!string.IsNullOrEmpty(ReferencedIn))
-                ReferencedIn += ";";
-            ReferencedIn += address;
+            UpdateRefs(ref _refdIn, ref _refdInCount, address);
         }
 
         public void AddCreatedIn(string address)
         {
-            CreatedInCount++;
-            if (!string.IsNullOrEmpty(CreatedIn))
-                CreatedIn += ";";
-            CreatedIn += address;
+            UpdateRefs(ref _createdIn, ref _createdInCount, address);
+        }
+
+        private static void UpdateRefs(ref string refs, ref int counts, string newRef)
+        {
+            if (string.IsNullOrEmpty(newRef))
+                return;
+
+            var existingRefs = refs.Split(_delimiter);
+            if (existingRefs.Contains(newRef))
+                return;
+
+            counts++;
+            if (existingRefs.Length > 0)
+                refs += _delimiter;
+            refs += newRef;
         }
     }
 }
