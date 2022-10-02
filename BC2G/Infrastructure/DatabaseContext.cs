b35130427ff2c561ff01b1da041f95df796d6b35
VJalili <VJalili@users.noreply.github.com>
2022-10-01 22:43:16 -0400

    Add methods to DatabaseContext for optimistic save.

--- BC2G/Infrastructure/DatabaseContext.cs
+++ BC2G/Infrastructure/DatabaseContext.cs
@@ -1,5 +1,9 @@
 ﻿using BC2G.Model;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Npgsql;
+using Polly;
+using Polly.Contrib.WaitAndRetry;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -53,5 +57,94 @@ namespace BC2G.Infrastructure
                 $"Username={_username};" +
                 $"Password={_password}");
         }*/
+
+        public static async Task OptimisticAddOrUpdate(
+            DatabaseContext context, 
+            IDbContextFactory<DatabaseContext> contextFactory, 
+            CancellationToken ct)
+        {
+            await OptimisticTx(async () =>
+            {
+                await context.SaveChangesAsync(ct);
+            }, 
+            async () =>
+            {
+                var enties = context.ChangeTracker.Entries<Utxo>();
+                var utxos = enties.Select(x => (Utxo)x.CurrentValues.ToObject());
+                await ResilientAddOrUpdate(utxos, contextFactory, ct);
+            });
+        }
+
+        public static async Task OptimisticAddOrUpdate(
+            ICollection<Utxo> utxos,
+            IDbContextFactory<DatabaseContext> contextFactory,
+            CancellationToken ct)
+        {
+            await OptimisticTx(async () =>
+            {
+                using var c = contextFactory.CreateDbContext();
+                await c.Utxos.AddRangeAsync(utxos, ct);
+                await c.SaveChangesAsync(ct);
+            },
+            async () =>
+            {
+                await ResilientAddOrUpdate(utxos, contextFactory, ct);
+            });
+        }
+
+        private static async Task OptimisticTx(Func<Task> tx, Func<Task> onFailure)
+        {
+            try { await tx(); }
+            catch (Exception e)
+            {
+                switch (e)
+                {
+                    case InvalidOperationException:
+                    case DbUpdateConcurrencyException:
+                    case DbUpdateException when (
+                        e.InnerException is PostgresException pe &&
+                        pe.SqlState == "23505"):
+                        await onFailure();
+                        break;
+
+                    default: throw;
+                }
+            }
+        }
+
+        private static async Task ResilientAddOrUpdate(
+            IEnumerable<Utxo> utxos,
+            IDbContextFactory<DatabaseContext> contextFactory,
+            CancellationToken ct)
+        {
+            foreach (var utxo in utxos)
+            {
+                var policy = Policy
+                    .Handle<InvalidOperationException>()
+                    .Or<DbUpdateConcurrencyException>()
+                    .Or<DbUpdateException>(
+                        e => e.InnerException is PostgresException pe &&
+                        pe.SqlState == "23505")
+                    .WaitAndRetryAsync(Backoff.DecorrelatedJitterBackoffV2(
+                        TimeSpan.FromSeconds(10), 3));
+
+                await policy.ExecuteAsync(async () =>
+                {
+                    using var c = contextFactory.CreateDbContext();
+                    var inDbUtxo = await c.Utxos.FindAsync(utxo.Id);
+                    if (inDbUtxo == null)
+                    {
+                        await c.Utxos.AddAsync(utxo, ct);
+                    }
+                    else
+                    {
+                        inDbUtxo.AddCreatedIn(utxo.CreatedIn);
+                        inDbUtxo.AddReferencedIn(utxo.ReferencedIn);
+                    }
+
+                    await c.SaveChangesAsync(ct);
+                });
+            }
+        }
     }
 }
