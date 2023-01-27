1eb5605e4e108071a7104a099462075abf632e4b
vjalili <VJalili@users.noreply.github.com>
2023-01-26 20:48:47 -0500

    Draft interfaces for node and edge.

---
+++ BC2G/Graph/Model/IEdge.cs
@@ -0,0 +1,15 @@
+﻿namespace BC2G.Graph.Model;
+
+public interface IEdge : IEdge<INode, INode>
+{ }
+
+public interface IEdge<out TSource, out TTarget>
+    where TSource : INode
+    where TTarget : INode
+{
+    public string Id { get; }
+    public TSource Source { get; }
+    public TTarget Target { get; }
+    public EdgeType Type { get; }
+    public double Value { get; }
+}
---
+++ BC2G/Graph/Model/INode.cs
@@ -0,0 +1,11 @@
+﻿namespace BC2G.Graph.Model;
+
+public interface INode
+{
+    public string Id { get; }
+    public int InDegree { get; }
+    public int OutDegree { get; }
+
+    public List<IEdge<INode, INode>> IncomingEdges { get; }
+    public List<IEdge<INode, INode>> OutgoingEdges { get; }
+}
