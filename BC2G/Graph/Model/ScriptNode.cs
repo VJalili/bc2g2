0c5f90b123ab5ce436d88442dad9ea33b00c4696
vjalili <VJalili@users.noreply.github.com>
2023-01-26 20:49:18 -0500

    Implement script node.

---
+++ BC2G/Graph/Model/ScriptNode.cs
@@ -0,0 +1,91 @@
+﻿namespace BC2G.Graph.Model;
+
+public class ScriptNode : Node, IComparable<ScriptNode>, IEquatable<ScriptNode>
+{
+    public string Address { get; } = BitcoinAgent.Coinbase;
+    public ScriptType ScriptType { get; } = ScriptType.Coinbase;
+
+    public static new string Header
+    {
+        get
+        {
+            return string.Join(Delimiter, new string[]
+            {
+                Node.Header,
+                "ScriptType"
+            });
+        }
+    }
+
+    /// <summary>
+    /// The coinbase node is created when the default value of id is used.
+    /// </summary>
+    /// <param name="id"></param>
+    public ScriptNode(string id = "0") : base(id) { }
+
+    public ScriptNode(string address, ScriptType scriptType) : this()
+    {
+        Address = address;
+        ScriptType = scriptType;
+    }
+
+    // TODO: This constructor should be removed, we should NOT set specifically.
+    public ScriptNode(string id, string address, ScriptType scriptType) : this(id)
+    {
+        Address = address;
+        ScriptType = scriptType;
+    }
+
+    public ScriptNode(Neo4j.Driver.INode node) :
+        this(node.ElementId,
+            (string)node.Properties[
+                Props.ScriptAddress.Name],
+
+            Enum.Parse<ScriptType>((string)node.Properties[
+                Props.ScriptType.Name]))
+    { }
+
+    public new double[] GetFeatures()
+    {
+        return
+            new double[] { (double)ScriptType }
+            .Concat(base.GetFeatures()).ToArray();
+    }
+
+    public override int GetHashCode()
+    {
+        // Do not add ID here, because ID is generated
+        // in a multi-threaded process, hence cannot
+        // guarantee a node's ID is reproducible.
+        return HashCode.Combine(Address, ScriptType);
+    }
+
+    public int CompareTo(ScriptNode? other)
+    {
+        if (other == null) return -1;
+        var r = Address.CompareTo(other.Address);
+        if (r != 0) return r;
+        return ScriptType.CompareTo(other.ScriptType);
+    }
+
+    public bool Equals(ScriptNode? other)
+    {
+        if (other == null)
+            return false;
+
+        return
+            Address == other.Address &&
+            ScriptType == other.ScriptType;
+    }
+
+    public override string ToString()
+    {
+        return string.Join(
+            Delimiter,
+            new string[]
+            {
+                base.ToString(),
+                ScriptType.ToString("d")
+            });
+    }
+}
