0490368421381afc2d3eff830d54c6dc6d3a2b7b
vjalili <VJalili@users.noreply.github.com>
2023-01-26 20:50:00 -0500

    Add TxNode.

---
+++ BC2G/Graph/Model/TxNode.cs
@@ -0,0 +1,21 @@
+﻿namespace BC2G.Graph.Model;
+
+public class TxNode : Node, IComparable<TxNode>, IEquatable<TxNode>
+{
+    public Transaction Tx { get; }    
+
+    public TxNode(Transaction tx) : base(tx.Txid)
+    {
+        Tx = tx;
+    }
+
+    public int CompareTo(TxNode? other)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Equals(TxNode? other)
+    {
+        throw new NotImplementedException();
+    }
+}
