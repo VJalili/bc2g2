be029cf00ae0b61b6ed130ad51016210848bc93e
vjalili <VJalili@users.noreply.github.com>
2023-01-31 22:20:50 -0500

    Add BitcoinMapperBase.

---
+++ BC2G/Graph/Db/Neo4j/BitcoinMappers/BitcoinMapperBase.cs
@@ -0,0 +1,20 @@
+﻿namespace BC2G.Graph.Db.Neo4j.BitcoinMappers;
+
+public abstract class BitcoinMapperBase<T> : IMapper<T>
+{
+    public const string csvDelimiter = "\t";
+    public const string labelsDelimiter = ":";
+
+    public static string CreatesEdgeQuery
+    {
+        get { return $"MERGE (block)-[:Creates {{{Props.Height.GetLoadExp(":")}}}]->(target) "; }
+    }
+    public static string RedeemsEdgeQuery
+    {
+        get { return $"MERGE (source)-[:Redeems {{{Props.Height.GetLoadExp(":")}}}]->(block) "; }
+    }
+
+    public abstract string GetCsv(T entity);
+    public abstract string GetCsvHeader();
+    public abstract string GetQuery(string csvFilename);
+}
