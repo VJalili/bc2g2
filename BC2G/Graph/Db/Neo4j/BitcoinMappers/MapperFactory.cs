4149eb708ef6ca8edcdfc65799416e35abdc03a9
vjalili <VJalili@users.noreply.github.com>
2023-01-31 22:20:36 -0500

    Add MapperFactory for bitcoin.

---
+++ BC2G/Graph/Db/Neo4j/BitcoinMappers/MapperFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace BC2G.Graph.Db.Neo4j.BitcoinMappers;
+
+public class MapperFactory : IMapperFactory
+{
+    public IMapper<T> Get<T>(T entity)
+    {
+        return typeof(T) switch
+        {
+            Type t when t == typeof(S2SEdge) => (IMapper<T>)new S2SEdgeMapper(),
+            _ => throw new NotImplementedException(),
+        };
+    }
+}
