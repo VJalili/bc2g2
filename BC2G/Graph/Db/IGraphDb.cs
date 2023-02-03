44b6b98fad1d9ad5af9941ec2dcfc09befc168b9
vjalili <VJalili@users.noreply.github.com>
2023-02-03 16:07:35 -0500

    extend interface.

--- BC2G/Graph/Db/IGraphDb.cs
+++ BC2G/Graph/Db/IGraphDb.cs
@@ -1,6 +1,7 @@
 ﻿namespace BC2G.Graph.Db;
 
-public interface IGraphDb<T> where T : GraphBase
+public interface IGraphDb<T> : IDisposable where T : GraphBase
 {
-    public void Import(T graph);
+    public void Serialize(T graph);
+    public void Import();
 }
