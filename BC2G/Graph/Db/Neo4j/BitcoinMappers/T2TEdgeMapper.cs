cf240fe38a23ec130fb6e56b28a2a00d7cd65365
vjalili <VJalili@users.noreply.github.com>
2023-02-06 21:42:37 -0500

    Add the cypher script to the T2TEdge mapper.

--- BC2G/Graph/Db/Neo4j/BitcoinMappers/T2TEdgeMapper.cs
+++ BC2G/Graph/Db/Neo4j/BitcoinMappers/T2TEdgeMapper.cs
@@ -31,8 +31,8 @@ public class T2TEdgeMapper : BitcoinMapperBase
     {
         return string.Join(csvDelimiter, new string[]
         {
-            edge.Source.Tx.Txid,
-            edge.Target.Tx.Txid,
+            edge.Source.Tx is not null? edge.Source.Tx.Txid : edge.Source.Id,
+            edge.Target.Tx is not null? edge.Target.Tx.Txid : edge.Target.Id,
             edge.Type.ToString(),
             edge.Value.ToString(),
             edge.BlockHeight.ToString()
@@ -41,6 +41,54 @@ public class T2TEdgeMapper : BitcoinMapperBase
 
     public override string GetQuery(string csvFilename)
     {
-        throw new NotImplementedException();
+        var l = Property.lineVarName;
+        var unknown = nameof(ScriptType.Unknown);
+
+        return
+           $"LOAD CSV WITH HEADERS FROM '{csvFilename}' AS {l} " +
+           $"FIELDTERMINATOR '{csvDelimiter}' " +
+           // Load source
+           $"MERGE (source:{labels} {{" +
+           $"{Props.T2TEdgeSourceTxid.GetLoadExp(":")}}}) " +
+           $"ON CREATE SET source.{Props.T2TEdgeSourceTxid.GetLoadExp("=")} " +
+           $"ON MATCH SET source.{Props.T2TEdgeSourceTxid.Name} = " +
+           $"CASE {l}.{Props.T2TEdgeSourceTxid.CsvHeader} " +
+           $"WHEN '{unknown}' THEN source.{Props.T2TEdgeSourceTxid.Name} " +
+           $"ELSE {l}.{Props.T2TEdgeSourceTxid.CsvHeader} " +
+           $"END " +
+           // Load target
+           $"MERGE (target:{labels} {{" +
+           $"{Props.T2TEdgeTargetTxid.GetLoadExp(":")}}}) " +
+           $"ON CREATE SET target.{Props.T2TEdgeTargetTxid.GetLoadExp("=")} " +
+           $"ON MATCH SET target.{Props.T2TEdgeTargetTxid.Name} = " +
+           $"CASE {l}.{Props.T2TEdgeTargetTxid.CsvHeader} " +
+           $"WHEN '{unknown}' THEN target.{Props.T2TEdgeTargetTxid.Name} " +
+           $"ELSE {l}.{Props.T2TEdgeTargetTxid.CsvHeader} " +
+           $"END " +
+           $"WITH source, target, {l} " +
+           // Find the block
+           $"MATCH (block:{BlockMapper.label} {{" +
+           $"{Props.Height.GetLoadExp(":")}" +
+           "}) " +
+           // Create relationship between the block node and the Tx nodes. 
+           RedeemsEdgeQuery +
+           CreatesEdgeQuery +
+           $"WITH source, target, {l} " +
+           // Create relationship between the source and target Tx nodes,
+           // where the type of the relationship is read from the CSV file.
+           "CALL apoc.merge.relationship(" +
+           "source, " + // source
+           $"{l}.{Props.EdgeType.CsvHeader}, " + // relationship type
+           $"{{" + // properties
+           $"{Props.EdgeValue.GetLoadExp(":")}, " +
+           $"{Props.Height.GetLoadExp(":")}" +
+           $"}}, " +
+           $"{{ Count : 0}}, " + // on create
+           $"target, " + // target
+           $"{{}}" + // on update
+           $")" +
+           $"YIELD rel " +
+           $"SET rel.Count = rel.Count + 1 " +
+           $"RETURN distinct 'DONE'";
     }
 }
