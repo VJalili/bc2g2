1ab58d0d2cb80d8c5c7b66fc9f0d959f49c55f0c
vjalili <VJalili@users.noreply.github.com>
2023-02-10 20:48:11 -0500

    Implement mapper not implemented exception.

---
+++ BC2G/Exceptions/MapperNotImplementedException.cs
@@ -0,0 +1,8 @@
+﻿namespace BC2G.Exceptions;
+
+internal class MapperNotImplementedException : NotImplementedException
+{
+    public MapperNotImplementedException(string type) :
+        base($"A mapper for type {type} is not implemented.")
+    { }
+}
