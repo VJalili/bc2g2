ac8d84bcf69f6567ab6ed8a8fabc48c64ecac662
vjalili <VJalili@users.noreply.github.com>
2023-02-11 17:52:36 -0500

    add type.

---
+++ BC2G/Blockchains/Bitcoin/C2TEdge.cs
@@ -0,0 +1,11 @@
+﻿namespace BC2G.Blockchains.Bitcoin;
+
+public class C2TEdge : T2TEdge
+{
+    public C2TEdge(
+        TxNode target, double value, uint timestamp, long blockHeight) :
+        base(
+            TxNode.GetCoinbaseNode(), target,
+            value, EdgeType.Generation, timestamp, blockHeight)
+    { }
+}
