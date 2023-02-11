72fb65bbc8417433862b177084418ed9b6715b6f
vjalili <VJalili@users.noreply.github.com>
2023-02-10 20:53:02 -0500

    Implement the IMapper interface in the MapperFactory.

--- BC2G/Graph/Db/Neo4j/BitcoinMappers/MapperFactory.cs
+++ BC2G/Graph/Db/Neo4j/BitcoinMappers/MapperFactory.cs
@@ -1,8 +1,21 @@
-﻿namespace BC2G.Graph.Db.Neo4j.BitcoinMappers;
+﻿using BC2G.Exceptions;
+
+namespace BC2G.Graph.Db.Neo4j.BitcoinMappers;
 
 public class MapperFactory : IMapperFactory
 {
-    public IMapper Get(string type)
+    public IMapperBase GetMapperBase(string type)
+    {
+        try { return GetEdgeMapper(type); }
+        catch (MapperNotImplementedException) { }
+
+        try { return GetGraphMapper(type); }
+        catch (MapperNotImplementedException) { }
+
+        throw new MapperNotImplementedException(type);
+    }
+
+    public IEdgeMapper GetEdgeMapper(string type)
     {
         if (type == null)
             throw new ArgumentNullException(nameof(type));
@@ -11,8 +24,20 @@ public class MapperFactory : IMapperFactory
         {
             string x when x == Utilities.TypeToString<S2SEdge>() => new S2SEdgeMapper(),
             string x when x == Utilities.TypeToString<T2TEdge>() => new T2TEdgeMapper(),
-            _ => throw new NotImplementedException(
-                $"A mapper for type {type} is not implemented."),
+            string x when x == Utilities.TypeToString<C2SEdge>() => new C2SEdgeMapper(),
+            _ => throw new MapperNotImplementedException(type)
+        };
+    }
+
+    public IGraphMapper GetGraphMapper(string type)
+    {
+        if (type == null)
+            throw new ArgumentNullException(nameof(type));
+
+        return type switch
+        {
+            string x when x == Utilities.TypeToString<BitcoinBlockGraph>() => new BitcoinBlockGraphMapper(),
+            _ => throw new MapperNotImplementedException(type)
         };
     }
 }
