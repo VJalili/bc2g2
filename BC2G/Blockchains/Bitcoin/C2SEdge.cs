7660efd2f27a7d7ede9f3276e2d6cd8d05371aea
vjalili <VJalili@users.noreply.github.com>
2023-02-11 17:52:25 -0500

    add type.

---
+++ BC2G/Blockchains/Bitcoin/C2SEdge.cs
@@ -0,0 +1,23 @@
+﻿namespace BC2G.Blockchains.Bitcoin;
+
+/// <summary>
+/// Coinbase to Script edge.
+/// This edge is implemented to simplify importing 
+/// Coinbase->Script edges into Neo4j by implementing
+/// Coinbase-specific logic and improvements.
+/// </summary>
+public class C2SEdge : S2SEdge
+{
+    public C2SEdge(
+        ScriptNode target, double value, uint timestamp, long blockHeight) :
+        base(
+            ScriptNode.GetCoinbaseNode(), target,
+            value, EdgeType.Generation, timestamp, blockHeight)
+    { }
+
+    public C2SEdge(
+        ScriptNode source, ScriptNode target,
+        IRelationship relationship) :
+        base(source, target, relationship)
+    { }
+}
