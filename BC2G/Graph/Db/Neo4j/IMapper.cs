b147460c01a222287c476e7ee6ee102ab170356f
vjalili <VJalili@users.noreply.github.com>
2023-02-10 20:51:25 -0500

    split imapper to IMapperBase, IEdgeMapper, and IGraphMapper.

--- BC2G/Graph/Db/Neo4j/IMapper.cs
+++ BC2G/Graph/Db/Neo4j/IMapper.cs
@@ -2,9 +2,29 @@
 
 namespace BC2G.Graph.Db.Neo4j;
 
-public interface IMapper
+public interface IMapperBase
 {
-    public string GetCsv(IEdge<INode, INode> entity);
     public string GetCsvHeader();
-    public string GetQuery(string csvFilename);
+
+    /// <summary>
+    /// The order of executing (de)serialization of 
+    /// types is not fixed, hence, the queries should 
+    /// be stateless and do not assume any precedence 
+    /// on existing types. For instance, a query creating
+    /// edges, should not assume nodes related to blocks
+    /// exist.
+    /// </summary>
+    public string GetQuery(string filename);
+}
+public interface IEdgeMapper : IMapperBase
+{
+    public string GetCsv(IEdge<INode, INode> edge);
+
+    public void ToCsv(IEnumerable<IEdge<INode, INode>> edges, string filename);
+}
+
+public interface IGraphMapper : IMapperBase
+{
+    public string GetCsv(GraphBase graph);
+    public void ToCsv(GraphBase graph, string filename);
 }
