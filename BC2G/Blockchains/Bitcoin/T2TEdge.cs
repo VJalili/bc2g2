c268d9f30b2b24d736df86deec80a215a32a033a
vjalili <VJalili@users.noreply.github.com>
2023-02-14 21:00:13 -0500

    extend tx node in t2tedge

--- BC2G/Blockchains/Bitcoin/T2TEdge.cs
+++ BC2G/Blockchains/Bitcoin/T2TEdge.cs
@@ -10,13 +10,32 @@ public class T2TEdge : Edge<TxNode, TxNode>
 
     public static T2TEdge Update(T2TEdge oldEdge, T2TEdge newEdge)
     {
+        /*
         var sourceTx = newEdge.Source.Tx ?? oldEdge.Source.Tx;
         var sourceTxid = newEdge.Source.Id ?? oldEdge.Source.Id;
-        var source = sourceTx is not null ? new TxNode(sourceTx) : new TxNode(sourceTxid);
+        var source = sourceTx is not null ? new TxNode(sourceTx) : new TxNode(sourceTxid);*/
 
+        var source = new TxNode(
+            newEdge.Source.Id,
+            newEdge.Source.Txid ?? oldEdge.Source.Txid,
+            newEdge.Source.Version ?? oldEdge.Source.Version,
+            newEdge.Source.Size ?? oldEdge.Source.Size,
+            newEdge.Source.VSize ?? oldEdge.Source.VSize,
+            newEdge.Source.Weight ?? oldEdge.Source.Weight,
+            newEdge.Source.LockTime ?? oldEdge.Source.LockTime);
+        /*
         var targetTx = newEdge.Target.Tx ?? oldEdge.Target.Tx;
         var targetTxid = newEdge.Target.Id ?? oldEdge.Target.Id;
-        var target = targetTx is not null ? new TxNode(targetTx) : new TxNode(targetTxid);
+        var target = targetTx is not null ? new TxNode(targetTx) : new TxNode(targetTxid);*/
+
+        var target = new TxNode(
+            newEdge.Target.Id,
+            newEdge.Target.Txid ?? oldEdge.Target.Txid,
+            newEdge.Target.Version ?? oldEdge.Target.Version,
+            newEdge.Target.Size ?? oldEdge.Target.Size,
+            newEdge.Target.VSize ?? oldEdge.Target.VSize,
+            newEdge.Target.Weight ?? oldEdge.Target.Weight,
+            newEdge.Target.LockTime ?? oldEdge.Target.LockTime);
 
         return new T2TEdge(
             source, target,
