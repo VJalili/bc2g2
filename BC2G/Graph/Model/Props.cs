3b527eb33e551097f2b2584f047f59de7cd9767b
vjalili <VJalili@users.noreply.github.com>
2023-02-14 21:05:41 -0500

    left from previous commit.

--- BC2G/Graph/Model/Props.cs
+++ BC2G/Graph/Model/Props.cs
@@ -2,14 +2,20 @@
 
 public static class Props
 {
-    private const string _txid = "Txid";
+    private const string _txNodeTxid = "Txid";
+    private const string _txNodeVersion = "Version";
+    private const string _txNodeSize = "Size";
+    private const string _txNodeVSize = "VSize";
+    private const string _txNodeWeight = "Weight";
+    private const string _txNodeLockTime = "LockTime";
+
     private const string _addressProperty = "Address";
     private const string _scriptTypeProperty = "ScriptType";
 
     public static Property Height { get; } = new("Height", FieldType.Int);
     public static Property ScriptAddress { get; } = new(_addressProperty);
     public static Property ScriptType { get; } = new(_scriptTypeProperty);
-    public static Property Txid { get; } = new(_txid);
+    public static Property Txid { get; } = new(_txNodeTxid);
     public static Property BlockMedianTime { get; } = new("MedianTime");
     public static Property BlockConfirmations { get; } = new("Confirmations", FieldType.Int);
     public static Property BlockDifficulty { get; } = new("Difficulty", FieldType.Float);
@@ -31,6 +37,16 @@ public static class Props
     public static Property EdgeTargetType { get; } = new(_scriptTypeProperty, csvHeader: "TargetType");
     public static Property EdgeType { get; } = new("EdgeType");
     public static Property EdgeValue { get; } = new("Value", FieldType.Float);
-    public static Property T2TEdgeSourceTxid { get; } = new Property(_txid, csvHeader: "SourceTxid");
-    public static Property T2TEdgeTargetTxid { get; } = new Property(_txid, csvHeader: "TargetTxid");
+    public static Property T2TEdgeSourceTxid { get; } = new Property(_txNodeTxid, csvHeader: "SourceId");
+    public static Property T2TEdgeSourceVersion { get; } = new Property(_txNodeVersion, FieldType.Int, "SourceVersion");
+    public static Property T2TEdgeSourceSize { get; } = new Property(_txNodeSize, FieldType.Int, "SourceSize");
+    public static Property T2TEdgeSourceVSize { get; } = new Property(_txNodeVSize, FieldType.Int, "SourceVSize");
+    public static Property T2TEdgeSourceWeight { get; } = new Property(_txNodeWeight, FieldType.Int, "SourceWeight");
+    public static Property T2TEdgeSourceLockTime { get; } = new Property(_txNodeLockTime, FieldType.Int, "SourceLockTime");
+    public static Property T2TEdgeTargetTxid { get; } = new Property(_txNodeTxid, csvHeader: "TargetId");
+    public static Property T2TEdgeTargetVersion { get; } = new Property(_txNodeVersion, FieldType.Int, "TargetVersion");
+    public static Property T2TEdgeTargetSize { get; } = new Property(_txNodeSize, FieldType.Int, "TargetSize");
+    public static Property T2TEdgeTargetVSize { get; } = new Property(_txNodeVSize, FieldType.Int, "TargetVSize");
+    public static Property T2TEdgeTargetWeight { get; } = new Property(_txNodeWeight, FieldType.Int, "TargetWeight");
+    public static Property T2TEdgeTargetLockTime { get; } = new Property(_txNodeLockTime, FieldType.Int, "TargetLockTime");
 }
