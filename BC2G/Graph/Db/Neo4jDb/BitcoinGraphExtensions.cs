5db6b010809ce162b1d05ea7670b5b8c0c570d9a
vjalili <VJalili@users.noreply.github.com>
2023-02-25 14:53:04 -0500

    Add BitcoinGraph extension that includes Neo4j related extensions.

---
+++ BC2G/Graph/Db/Neo4jDb/BitcoinGraphExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace BC2G.Graph.Db.Neo4jDb;
+
+internal static class BitcoinGraphExtensions
+{
+    public static S2SEdge GetOrAddEdge(this GraphBase g, IRelationship e)
+    {
+        var source = g.GetOrAddNode(new ScriptNode(e.StartNodeElementId));
+        var target = g.GetOrAddNode(new ScriptNode(e.EndNodeElementId));
+
+        var edge = g.GetOrAddEdge(new S2SEdge(source, target, e));
+
+        source.AddOutgoingEdges(edge);
+        target.AddIncomingEdges(edge);
+
+        return edge;
+    }
+}
