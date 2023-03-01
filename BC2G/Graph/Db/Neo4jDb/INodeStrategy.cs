ddda91b2fe47190bcc049f11481e971d3a46d048
vjalili <VJalili@users.noreply.github.com>
2023-02-28 21:32:09 -0500

    add INodeStrategy.

---
+++ BC2G/Graph/Db/Neo4jDb/INodeStrategy.cs
@@ -0,0 +1,8 @@
+﻿namespace BC2G.Graph.Db.Neo4jDb;
+
+public interface INodeStrategy : IStrategyBase
+{
+    public string GetCsv(Model.INode node);
+
+    public void ToCsv(IEnumerable<Model.INode> nodes, string filename);
+}
