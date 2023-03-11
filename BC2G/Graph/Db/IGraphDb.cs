9d621d743175d88652add2444a24ee1c934873fd
vjalili <VJalili@users.noreply.github.com>
2023-03-11 12:08:03 -0500

    Add import order to the Import method of IGraphDb.

--- BC2G/Graph/Db/IGraphDb.cs
+++ BC2G/Graph/Db/IGraphDb.cs
@@ -3,6 +3,6 @@
 public interface IGraphDb<T> : IDisposable where T : GraphBase
 {
     public Task SerializeAsync(T graph, CancellationToken ct);
-    public Task ImportAsync(string batchName = "");
+    public Task ImportAsync(string batchName = "", List<string>? importOrder = null);
     public Task<bool> TrySampleAsync();
 }
