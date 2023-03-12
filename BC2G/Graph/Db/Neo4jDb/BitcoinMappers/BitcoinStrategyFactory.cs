465941693f6c0309c5dc4cd5168cbe8d9b58f6a7
vjalili <VJalili@users.noreply.github.com>
2023-03-12 16:11:46 -0400

    remove commented out code.

--- BC2G/Graph/Db/Neo4jDb/BitcoinMappers/BitcoinStrategyFactory.cs
+++ BC2G/Graph/Db/Neo4jDb/BitcoinMappers/BitcoinStrategyFactory.cs
@@ -20,13 +20,9 @@ public class BitcoinStrategyFactory : IStrategyFactory
 
     public IGraphStrategy GetGraphStrategy(GraphComponentType type)
     {
-        /*if (type == null)
-            throw new ArgumentNullException(nameof(type));*/
-
         return type switch
         {
             GraphComponentType.BitcoinGraph => new BlockGraphStrategy(),
-            //string x when x == Utilities.TypeToString<BlockGraph>() => new BlockGraphStrategy(),
             _ => throw new MapperNotImplementedException(type)
         };
     }
@@ -35,9 +31,7 @@ public class BitcoinStrategyFactory : IStrategyFactory
     {
         return type switch
         {
-            //string x when x == Utilities.TypeToString<ScriptNode>() => new ScriptNodeStrategy(),
             GraphComponentType.BitcoinScriptNode => new ScriptNodeStrategy(),
-            //string x when x == Utilities.TypeToString<TxNode>() => new TxNodeStrategy(),
             GraphComponentType.BitcoinTxNode => new TxNodeStrategy(),
             _ => throw new MapperNotImplementedException(type)
         };
@@ -45,18 +39,11 @@ public class BitcoinStrategyFactory : IStrategyFactory
 
     public IEdgeStrategy GetEdgeStrategy(GraphComponentType type)
     {
-        /*if (type == null)
-            throw new ArgumentNullException(nameof(type));*/
-
         return type switch
         {
-            //string x when x == Utilities.TypeToString<C2SEdge>() => new C2SEdgeStrategy(),
             GraphComponentType.BitcoinC2S => new C2SEdgeStrategy(),
-            //string x when x == Utilities.TypeToString<C2TEdge>() => new C2TEdgeStrategy(),
             GraphComponentType.BitcoinC2T => new C2TEdgeStrategy(),
-            //string x when x == Utilities.TypeToString<S2SEdge>() => new S2SEdgeStrategy(),
             GraphComponentType.BitcoinS2S => new S2SEdgeStrategy(),
-            //string x when x == Utilities.TypeToString<T2TEdge>() => new T2TEdgeStrategy(),
             GraphComponentType.BitcoinT2T => new T2TEdgeStrategy(),
             _ => throw new MapperNotImplementedException(type)
         };
