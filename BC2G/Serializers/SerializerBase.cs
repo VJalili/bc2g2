bc819ea082d0d47ad760664d1a8dbcdbe1be4149
vjalili <VJalili@users.noreply.github.com>
2023-03-14 22:58:30 -0400

    Remove the unneeded CSV serializers.

--- BC2G/Serializers/SerializerBase.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BC2G.Serializers;
-
-public abstract class SerializerBase
-{
-    public abstract void Serialize(BlockGraph g, string baseFilename);
-
-    public abstract BlockGraph Deserialize(string path, int blockHeight);
-}
