a82579ed7bea00389a8fd101d22841e1fa4a74d1
vjalili <VJalili@users.noreply.github.com>
2023-03-25 15:38:15 -0400

    Add edge query using Apoc Create & modify Creates and Redeems edges to be CREATE not MERGE for performance reasons.

--- BC2G/Graph/Db/Neo4jDb/BitcoinStrategies/BitcoinStrategyBase.cs
+++ BC2G/Graph/Db/Neo4jDb/BitcoinStrategies/BitcoinStrategyBase.cs
@@ -4,35 +4,107 @@ public abstract class BitcoinEdgeStrategy : StrategyBase
 {
     public static string GetCreatesEdgeQuery(string blockVar = "block", string targetVar = "target")
     {
-        return $"MERGE ({blockVar})-[:Creates {{{Props.Height.GetSetter()}}}]->({targetVar})";
+        // The following is an example of the query this method generates.
+        //
+        // CREATE (block)-[:Creates {Height:toInteger(line.Height), Value:toFloat(line.Value)}]->(target)
+        //
+
+        var builder = new StringBuilder(
+            $"CREATE ({blockVar})-[:{Property.createsEdgeLabel} {{");
+
+        builder.Append(
+            string.Join(", ", from x in GetEdgePropertiesBase() select x.GetSetter()));
+
+        builder.Append(
+            $"}}]->({targetVar})");
+
+        return builder.ToString();
     }
 
     public static string GetRedeemsEdgeQuery(string blockVar = "block", string sourceVar = "source")
     {
-        return $"MERGE ({sourceVar})-[:Redeems {{{Props.Height.GetSetter()}}}]->({blockVar})";
+        // The following is an example of the query this method generates.
+        //
+        // CREATE (source)-[:Redeems {Height:toInteger(line.Height), Value:toFloat(line.Value)}]->(block)
+        //
+
+        var builder = new StringBuilder(
+            $"CREATE ({sourceVar})-[:{Property.redeemsEdgeLabel} {{");
+
+        builder.Append(
+            string.Join(", ", from x in GetEdgePropertiesBase() select x.GetSetter()));
+
+        builder.Append(
+            $"}}]->({blockVar})");
+
+        return builder.ToString();
     }
 
-    public static string GetEdgeQuery(
-        List<Property> props,
+    public static ReadOnlyCollection<Property> GetEdgePropertiesBase()
+    {
+        return new ReadOnlyCollection<Property>(new Property[] { Props.Height, Props.EdgeValue });
+    }
+
+    public static string GetApocCreateEdgeQuery(
+        IEnumerable<Property> props,
         string sourceVar = "source",
         string targetVar = "target")
     {
-        // An example of the populated template
-        // (indentation added for better readibility).
+        // The following is an example of the query this method generates.
+        // Indentation is added for better readability and does not exist 
+        // in the generated query.
         //
-        // CALL apoc.merge.relationship(
+        // CALL apoc.create.relationship(
         //     source,
         //     line.EdgeType,
         //     {
         //         Value:toFloat(line.Value),
         //         Height:toInteger(line.Height)
         //     },
-        //     { Count : 0},
+        //     target)
+        // YIELD rel
+        //
+
+        var builder = new StringBuilder(
+            $"CALL apoc.create.relationship(" +
+            $"{sourceVar}, " +
+            $"{Property.lineVarName}.{Props.EdgeType.CsvHeader}, " +
+            $"{{");
+
+        builder.Append(string.Join(", ", from x in props select x.GetSetter()));
+
+        builder.Append(
+            $"}}, " +
+            $"{targetVar}) " +
+            $"YIELD rel");
+
+        return builder.ToString();
+    }
+
+    public static string GetApocMergeEdgeQuery(
+        List<Property> props,
+        string sourceVar = "source",
+        string targetVar = "target")
+    {
+        // The following is an example of the query this method generates.
+        // Indentation added for better readibility and does not exist 
+        // in the generated query.
+        //
+        // CALL apoc.merge.relationship(
+        //     coinbase,
+        //     line.EdgeType,
+        //     {
+        //         Value:toFloat(line.Value),
+        //         Height:toInteger(line.Height)
+        //     },
+        //     {
+        //         Count : 0
+        //     },
         //     target,
-        //     {}
-        // )
-        // YIELD rel SET rel.Count = rel.Count + 1
-        // 
+        //     {})
+        // YIELD rel
+        // SET rel.Count = rel.Count + 1
+        //
 
         var builder = new StringBuilder(
             "CALL apoc.merge.relationship(" +
