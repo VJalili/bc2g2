6e064cb5829a73166414c01083726231ef1da5be
vjalili <VJalili@users.noreply.github.com>
2023-09-02 20:21:08 -0400

    Add DBContextFactory in order to enable creating migration scripts, without needing param-less constructor.

---
+++ BC2G/Infrastructure/DatabaseContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+
+namespace BC2G.Infrastructure;
+
+public class DatabaseContextFactory : IDesignTimeDbContextFactory<DatabaseContext>
+{
+    public DatabaseContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<DatabaseContext>();
+
+        var psqlDefaults = new PsqlOptions();
+        optionsBuilder.UseNpgsql(
+                $"Host={psqlDefaults.Host};" +
+                $"Database={psqlDefaults.Database};" +
+                $"Username={psqlDefaults.Username};" +
+                $"Password={psqlDefaults.Password}");
+
+        return new DatabaseContext(optionsBuilder.Options);
+    }
+}
