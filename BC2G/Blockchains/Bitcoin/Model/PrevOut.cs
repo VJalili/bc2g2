d78c27cba2007b156297f5097f168584f0ea2779
VJalili <VJalili@users.noreply.github.com>
2024-11-09 15:09:22 -0500

    add prev out type

---
+++ BC2G/Blockchains/Bitcoin/Model/PrevOut.cs
@@ -0,0 +1,26 @@
+﻿namespace BC2G.Blockchains.Bitcoin.Model;
+
+public class PrevOut
+{
+    [JsonPropertyName("generated")]
+    public bool Generated { set; get; }
+
+    [JsonPropertyName("height")]
+    public int Height { set; get; }
+
+    [JsonPropertyName("value")]
+    public double Value { set; get; }
+
+    [JsonPropertyName("scriptPubKey")]
+    public ScriptPubKey? ScriptPubKey { set; get; }
+
+    public Output ConstructedOutput { init;  get; }
+
+    public PrevOut()
+    {
+        ConstructedOutput = new Output()
+        {
+            ScriptPubKey = ScriptPubKey
+        };
+    }
+}
