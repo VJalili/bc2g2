5708803a809b5a8f6f073e37fba1145bcb606b72
VJalili <VJalili@users.noreply.github.com>
2025-03-04 21:06:51 -0500

    add ForestFireSampling algorithm

--- BC2G/Graph/Db/Neo4jDb/BitcoinNeo4jDb.cs
+++ BC2G/Graph/Db/Neo4jDb/BitcoinNeo4jDb.cs
@@ -3,6 +3,12 @@ using BC2G.Graph.Db.Neo4jDb.BitcoinStrategies;
 
 using Microsoft.Extensions.Primitives;
 
+using NBitcoin.RPC;
+
+using System.Collections.Generic;
+using System.Linq;
+
+
 namespace BC2G.Graph.Db.Neo4jDb;
 
 public class BitcoinNeo4jDb : Neo4jDb<BitcoinGraph>
@@ -129,6 +135,8 @@ public class BitcoinNeo4jDb : Neo4jDb<BitcoinGraph>
     {
         var graph = await GetNeighborsAsync(driver, rootNode.Address, Options.GraphSample);
 
+        //graph.DownSample(200, 50000);
+
         if (!CanUseGraph(
             graph, tolerance: 0,
             minNodeCount: Options.GraphSample.MinNodeCount,
@@ -173,6 +181,15 @@ public class BitcoinNeo4jDb : Neo4jDb<BitcoinGraph>
     public override async Task<GraphBase> GetNeighborsAsync(
         IDriver driver, string rootScriptAddress, GraphSampleOptions options)
     {
+        // BFS or DFS algorithms
+        //return await GetNeighborsUsingSpanningTreeAsync(driver, rootScriptAddress, options);
+
+        // forest fire sampling.
+        return await GetNeighborsUsingForestFireSamplingAlgorithmAsync(driver, rootScriptAddress, options);
+    }
+
+    private async Task<GraphBase> GetNeighborsUsingGraphTraversalAlgorithmAsync(IDriver driver, string rootScriptAddress, GraphSampleOptions options)
+    {
         // TODO: the whole method of using 'Coinbase' to alter the functionality seems hacky
         // need to find a better solution.
 
@@ -189,7 +206,7 @@ public class BitcoinNeo4jDb : Neo4jDb<BitcoinGraph>
         qBuilder.Append($"CALL apoc.path.spanningTree(root, {{");
         qBuilder.Append($"maxLevel: {options.Hops}, ");
         qBuilder.Append($"limit: {Options.GraphSample.MaxEdgesFetchFromNeighbor}, ");
-        
+
         if (Options.GraphSample.PathSearchAlgorith == PathSearchAlgorith.BFS)
             qBuilder.Append($"bfs: true, ");
         else
@@ -273,6 +290,161 @@ public class BitcoinNeo4jDb : Neo4jDb<BitcoinGraph>
 
         return g;
     }
+
+    private async Task<GraphBase> GetNeighborsUsingForestFireSamplingAlgorithmAsync(IDriver driver, string rootScriptAddress, GraphSampleOptions options)
+    {
+        // TODO: this method is experimental, need a thorough re-write.
+
+        var nodeSamplingCountAtRoot = 15;
+        var rnd = new Random(31);
+        var g = new BitcoinGraph();
+        var maxHops = 4;
+        var nodeCountReductionFactorByHop = 4.0;
+        var allNodesAddedToGraph = new HashSet<string>();
+        var allEdgesAddedToGraph = new HashSet<string>();
+        using var session = driver.AsyncSession(x => x.WithDefaultAccessMode(AccessMode.Read));
+
+        string GetQuery(string rootNode)
+        {
+            var qBuilder = new StringBuilder();
+            qBuilder.Append(rootNode);
+
+            qBuilder.Append($"CALL apoc.path.spanningTree(root, {{");
+            qBuilder.Append($"maxLevel: 1, ");
+            qBuilder.Append($"limit: 10000, ");
+            qBuilder.Append($"bfs: true, ");
+            qBuilder.Append($"labelFilter: '{ScriptNodeStrategy.Labels}'");
+            //$"    relationshipFilter: \">{EdgeType.Transfers}\"" +
+            qBuilder.Append($"}}) ");
+            qBuilder.Append($"YIELD path ");
+            qBuilder.Append($"WITH root, ");
+            qBuilder.Append($"nodes(path) AS pathNodes, ");
+            qBuilder.Append($"relationships(path) AS pathRels ");
+            qBuilder.Append($"LIMIT 10000 ");
+            qBuilder.Append($"RETURN [root] AS root, [n IN pathNodes WHERE n <> root] AS nodes, pathRels AS relationships");
+
+            return qBuilder.ToString();
+        }
+
+        List<Model.INode> ProcessSamplingResult(List<IRecord> samplingResult, int hop)
+        {
+            Node root;
+            var nodes = new Dictionary<string, Neo4j.Driver.INode>();
+            var edges = new Dictionary<string, IRelationship>();
+
+            // TODO: this iteration needs to be improved, maybe I have a list like this because of the query?!
+            foreach (var r in samplingResult)
+            {
+                if (rootScriptAddress == BitcoinAgent.Coinbase)
+                {
+                    root = new CoinbaseNode(r.Values["root"].As<List<Neo4j.Driver.INode>>()[0]);
+
+                    if (root is null)
+                        continue;
+
+                    if (!allNodesAddedToGraph.Contains(root.Id))
+                    {
+                        g.GetOrAddNode(GraphComponentType.BitcoinCoinbaseNode, root);
+                        allNodesAddedToGraph.Add(root.Id);
+                    }
+                }
+                else
+                {
+                    var rootB = r.Values["root"].As<List<Neo4j.Driver.INode>>()[0];
+
+                    if (rootB is null)
+                        continue;
+
+                    if (!allNodesAddedToGraph.Contains(rootB.ElementId))
+                    {
+                        g.GetOrAddNode(rootB);
+                        allNodesAddedToGraph.Add(rootB.ElementId);
+                    }
+                }
+
+                foreach (var node in r.Values["nodes"].As<List<Neo4j.Driver.INode>>())
+                    if (!allNodesAddedToGraph.Contains(node.ElementId))
+                        nodes.TryAdd(node.ElementId, node);
+
+                foreach (var edge in r.Values["relationships"].As<List<IRelationship>>())
+                    if (!allEdgesAddedToGraph.Contains(edge.ElementId))
+                        edges.TryAdd(edge.ElementId, edge);
+            }
+
+            var nodesToKeep = nodes.Keys.OrderBy(x => rnd.Next()).Take((int)Math.Floor(nodeSamplingCountAtRoot - (hop * nodeCountReductionFactorByHop))).ToList();
+            var nodesToKeepIds = new HashSet<string>();
+            foreach (var nodeId in nodesToKeep)
+                if (!allNodesAddedToGraph.Contains(nodeId))
+                    nodesToKeepIds.Add(nodeId);
+
+            var addedNodes = new List<Model.INode>();
+
+            foreach (var edge in edges)
+            {
+                var targetNodeId = edge.Value.EndNodeElementId;
+                if (nodesToKeepIds.Contains(targetNodeId))
+                {
+                    // so only the "connected" nodes are added.
+                    // also, this order is important where 1st the node is added, then the edge.
+                    addedNodes.Add(g.GetOrAddNode(nodes[targetNodeId]));
+                    allNodesAddedToGraph.Add(targetNodeId);
+
+                    g.GetOrAddEdge(edge.Value);
+                    allEdgesAddedToGraph.Add(edge.Value.ElementId);
+                }
+            }
+
+            return addedNodes;
+        }
+
+        async Task ProcessHops(List<string> queries, int hop = 0)
+        {
+            foreach (var q in queries)
+            {
+                var samplingResult = await session.ExecuteReadAsync(async x =>
+                {
+                    var result = await x.RunAsync(q);
+                    return await result.ToListAsync();
+                });
+
+                var selectedNodes = ProcessSamplingResult(samplingResult, hop);
+
+                if (hop < maxHops)
+                {
+                    var qs = new List<string>();
+                    foreach (var node in selectedNodes)
+                        qs.Add(GetQuery($"MATCH (root:{ScriptNodeStrategy.Labels} {{ Address: \"{((ScriptNode)node).Address}\" }}) "));
+
+                    await ProcessHops(qs, hop + 1);
+                }
+            }
+        }
+
+        // TODO: the whole method of using 'Coinbase' to alter the functionality seems hacky
+        // need to find a better solution.
+
+        Logger.LogInformation("Getting neighbors of random node {node}, at {hop} hop distance.", rootScriptAddress, options.Hops);
+
+
+        var queries = new List<string>()
+        {
+            GetQuery(
+                rootScriptAddress == BitcoinAgent.Coinbase ?
+                $"MATCH (root:{BitcoinAgent.Coinbase}) " :
+                $"MATCH (root:{ScriptNodeStrategy.Labels} {{ Address: \"{rootScriptAddress}\" }}) ")
+        };
+
+        await ProcessHops(queries);
+
+        Logger.LogInformation("Retrieved neighbors.");
+        Logger.LogInformation("Building a graph from the neighbors.");
+
+        Logger.LogInformation("Build graph from the neighbors; {nodeCount} nodes and {edgeCount} edges.", g.NodeCount, g.EdgeCount);
+
+        return g;
+    }
+
+
 
     public override async Task<GraphBase> GetRandomEdges(
         IDriver driver, int edgeCount, double edgeSelectProb = 0.2)
